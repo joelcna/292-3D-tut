d6c36beb550d49c5ccde58778632c63a3c2ee085
joelcna <joelcna28@gmail.com>
2023-10-01 21:35:36 -0400

    1:45

--- 292 3D Tut/Assets/Scripts/GameManager.cs
+++ 292 3D Tut/Assets/Scripts/GameManager.cs
@@ -1,9 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    enum CurrentPlayer
+    {
+        Player1,
+        Player2
+    }
+
+    CurrentPlayer currentPlayer;
+    bool isWinningShotForPlayer1 = false;
+    bool isWinningShotForPlayer2 = false;
+    int player1BallsRemaining = 7;
+    int player2BallsRemaining = 7;
+
+    [SerializeField] TextMeshProUGUI player1BallsText;
+    [SerializeField] TextMeshProUGUI player2BallsText;
+    [SerializeField] TextMeshProUGUI currentTurnText;
+    [SerializeField] TextMeshProUGUI messageText;
+
+    [SerializeField] GameObject restartButton;
+
+    [SerializeField] GameObject headPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +38,105 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    bool Scratch()
+    {
+        if (currentPlayer == CurrentPlayer.Player1)
+        {
+            if (isWinningShotForPlayer1)
+            {
+                ScratchOnWinningShot("Player 1");
+                return true;
+            }
+        }
+        else
+        {
+            if (isWinningShotForPlayer2)
+            {
+                ScratchOnWinningShot("Player 2");
+                return true;
+            }
+        }
+        NextPlayerTurn();
+        return false;
+    }
+
+    void EarlyEightBall()
+    {
+        if (currentPlayer == CurrentPlayer.Player1)
+        {
+            Lose("Player 1 Has Sunk the 8 Ball Too Early and Has Lost the Game");
+        }
+        else
+        {
+            Lose("Player 2 Has Sunk the 8 Ball Too Early and Has Lost the Game");
+        }
+    }
+
+    void ScratchOnWinningShot(string player)
+    {
+        Lose(player + " Scratched on Their Final Shot and Has Lost");
+    }
+
+    void NoMoreBalls(CurrentPlayer player)
+    {
+        if (player == CurrentPlayer.Player1) 
+        {
+            isWinningShotForPlayer1 = true;
+        }
+        else
+        {
+            isWinningShotForPlayer2 = true;
+        }
+    }
+
+    bool CheckBall(Ball ball)
+    {
+        return true;
+    }
+
+    void Lose (string message)
+    {
+        messageText.gameObject.SetActive(true);
+        messageText.text = message;
+        restartButton.SetActive(true);
+    }
+
+    void Win(string player)
+    {
+        messageText.gameObject.SetActive(true);
+        messageText.text = player + " Has Won!";
+        restartButton.SetActive(true);
+    }
+
+    void NextPlayerTurn()
+    {
+        if (currentPlayer == CurrentPlayer.Player1)
+        {
+            currentPlayer = CurrentPlayer.Player2;
+            currentTurnText.text = "Current Turn: Player 2";
+        }
+        else
+        {
+            currentPlayer = CurrentPlayer.Player2;
+            currentTurnText.text = "Current Turn: Player 1";
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+            if (CheckBall(other.gameObject.GetComponent<Ball>()))
+            {
+                Destroy(other.gameObject);
+            }
+            else
+            {
+                other.gameObject.transform.position = headPosition.position;
+                other.gameObject.GetComponent<Rigidbody>()
+            }
+        }
     }
 }
